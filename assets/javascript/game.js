ad2721fee367ed1c9e89d368e9aacfb5fe07b945
Ashley <ashley_colleen@hotmail.com>
2020-05-05 19:00:19 -0400

    added javascript, css, and sound

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,8 +2,10 @@
 let teamone_numshots = document.querySelector("#teamone-numshots");
 let teamone_shoot_button = document.querySelector("#teamone-shoot-button");
 let teamone_numgoals = document.querySelector("#teamone-numgoals");
+let mySound = document.querySelector("#sound");
 
 teamone_shoot_button.addEventListener("click", function()  {
+    mySound.play();
 
     console.log ("+ button clicked")
 
@@ -12,15 +14,16 @@ teamone_shoot_button.addEventListener("click", function()  {
 
     teamone_numshots.innerHTML = shots_taken1;
 
-        
-    let teamone_goals = teamone_numgoals
+     
 
-    console.log(shots_taken1 = Math.random())
-    shots_taken1 = Math.random();
-    if(shots_taken1 > .5) {
-     teamone_goals = parseInt(teamone_numgoals.innerHTML) + 1;
+   
+    let randnum = Math.random();
+    console.log(randnum)
+    if(randnum > .5) {
+     let newGoals = parseInt(teamone_numgoals.innerHTML) + 1;
+        teamone_numgoals.innerHTML = newGoals
+     
    }
-     teamone_numgoals.innerHTML = teamone_goals;
     
 })
 
@@ -28,8 +31,10 @@ teamone_shoot_button.addEventListener("click", function()  {
 
 let teamtwo_numshots = document.querySelector("#teamtwo-numshots");
 let teamtwo_shoot_button = document.querySelector("#teamtwo-shoot-button");
+let teamtwo_numgoals = document.querySelector("#teamtwo-numgoals");
 
 teamtwo_shoot_button.addEventListener("click", function()  {
+    mySound.play();
 
     console.log ("+ button clicked")
 
@@ -38,22 +43,46 @@ teamtwo_shoot_button.addEventListener("click", function()  {
 
     teamtwo_numshots.innerHTML = shots_taken2;
 
+    let randnum = Math.random();
+    console.log(randnum)
+    if(randnum > .5) {
+     let newGoals = parseInt(teamtwo_numgoals.innerHTML) + 1;
+        teamtwo_numgoals.innerHTML = newGoals
+     
+   }
 
 })
 
 let reset_button = document.querySelector("#reset-button");
 let num_resets = document.querySelector("#num-resets");
+let gameWinner = document.querySelector(".winner-box");
 
 reset_button.addEventListener("click", function()  {
+    mySound.play();
+
 
     console.log ("+ button clicked")
 
     
     let reset_count = parseInt(num_resets.innerHTML) +1;
 
+
+    console.log(teamone_numgoals.innerText > teamtwo_numgoals.innerText)
+    if(teamone_numgoals.innerText > teamtwo_numgoals.innerText) {
+        gameWinner.innerText = "Team 1 WINS!"
+    }
+    else {
+        gameWinner.innerText ="Team 2 WINS!"
+    }
+
     num_resets.innerHTML = reset_count;
     teamone_numshots.innerHTML = 0;
     teamtwo_numshots.innerHTML = 0;
+    teamone_numgoals.innerHTML = 0;
+    teamtwo_numgoals.innerHTML = 0;
+
+
+
 })
 
 
